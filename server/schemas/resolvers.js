b30a1c52473a18ba61bd6bb4b794db631c19468d
Abstrack <Armon_ah05@outlook.com>
2022-07-25 21:33:06 -0400

    added definition/resolver for User/Users  and Thoughts/Thought

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,43 @@
+const { User, Thought } = require("../models");
+
+// Here, we pass in the parent as more of a placeholder parameter.
+// It won't be used, but we need something in that first parameter's spot
+// so we can access the username argument from the second parameter. We use
+// a ternary operator to check if username exists. If it does, we set params
+// to an object with a username key set to that value. If it doesn't,
+// we simply return an empty object.
+const resolvers = {
+  Query: {
+    thoughts: async (parent, { username }) => {
+      const params = username ? { username } : {}; //ternary operator to check if username exists.
+      return Thought.find(params).sort({ createdAt: -1 });
+    },
+    thought: async (parent, { _id }) => {
+      return Thought.findOne({ _id });
+    },
+    users: async () => {
+      return User.find()
+        .select('-__v -password')
+        .populate('friends')
+        .populate('thoughts');
+    },
+    user: async (parent, {username}) => {
+      return User.findOne({ username })
+        .select('-__v -password')
+        .populate('friends')
+        .populate('thoughts')
+    },
+  },
+};
+
+module.exports = resolvers;
+
+//A resolver can accept four arguments in the following order:
+
+// 1. parent: This is if we used nested resolvers to handle more complicated actions, as it would hold the reference to the resolver that executed the nested resolver function. We won't need this throughout the project, but we need to include it as the first argument.
+
+// 2. args: This is an object of all of the values passed into a query or mutation request as parameters. In our case, we destructure the username parameter out to be used.
+
+// 3. context: This will come into play later. If we were to need the same data to be accessible by all resolvers, such as a logged-in user's status or API access token, this data will come through this context parameter as an object.
+
+// 4. info: This will contain extra information about an operation's current state. This isn't used as frequently, but it can be implemented for more advanced uses.
---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,52 @@
+// import the gql tagged template function
+const { gql } = require("apollo-server-express");
+
+// create our typeDefs
+// To define a query, you use the type Query {} data type
+// type thought is custom data type. With this custom data
+// type, we are able to instruct the thoughts query so that each
+// thought that returns can include _id, thoughtText, username, and
+// reactionCount fields with their respective GraphQL scalars
+
+const typeDefs = gql`
+  type User {
+    _id: ID
+    username: String
+    email: String
+    friendCount: Int
+    thoughts: [Thought]
+    friends: [User]
+  }
+
+  type Thought {
+    _id: ID
+    thoughtText: String
+    createdAt: String
+    username: String
+    reactionCount: Int
+    reactions: [Reaction]
+  }
+
+  type Reaction {
+    _id: ID
+    reactionBody: String
+    createdAt: String
+    username: String
+  }
+
+  type Query {
+    users: [User]
+    user(username: String!): User
+    thoughts(username: String): [Thought]
+    thought(_id: ID!): Thought
+  }
+`;
+
+//Notice the exclamation point ! after the query parameter data type definitions? 
+// That indicates that for that query to be carried out, that data must exist. 
+// Otherwise, Apollo will return an error to the client making the request and the 
+// query won't even reach the resolver function associated with it.
+
+
+// export the typeDefs
+module.exports = typeDefs;
