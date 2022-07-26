2b6c04d43d5022a713b5cbc37f438ce0724f46e9
Abstrack <Armon_ah05@outlook.com>
2022-07-26 15:18:55 -0400

    added  code line to fix me query with jwt

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -12,7 +12,7 @@ const resolvers = {
   Query: {
     me: async (parent, args, context) => {
       if (context.user) {
-        const userData = await User.find({})
+        const userData = await User.findOne({ _id: context.user._id })
           .select("-__v -password")
           .populate("thoughts")
           .populate("friends");
