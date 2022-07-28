e071acd0447f47237f588e236cfe7b8d13a7b269
Abstrack <Armon_ah05@outlook.com>
2022-07-28 16:44:09 -0400

    added addFriend working component, working on adding thoughts, submit  clear/reset textarea and counts characters

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -2,13 +2,17 @@ import React from "react";
 import { Navigate, useParams } from "react-router-dom";
 import ThoughtList from "../components/ThoughtList";
 import FriendList from "../components/FriendList";
+import ThoughtForm from "../components/ThoughtForm";
 
-import { useQuery } from "@apollo/client";
+import { useQuery, useMutation } from "@apollo/client";
 import { QUERY_USER, QUERY_ME } from "../utils/queries";
+import { ADD_FRIEND } from "../utils/mutations";
 
 import Auth from "../utils/auth";
 
 const Profile = () => {
+  const [ addFriend ] = useMutation(ADD_FRIEND);
+
   const { username: userParam } = useParams();
   // The useParams Hook retrieves the username from the URL,
   // which is then passed to the useQuery Hook.
@@ -36,12 +40,28 @@ const Profile = () => {
     );
   }
 
+  const handleClick = async () => {
+    try {
+      await addFriend ({
+        variables: { id: user._id }
+      });
+
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
   return (
     <div>
       <div className="flex-row mb-3">
         <h2 className="bg-dark text-secondary p-3 display-inline-block">
           Viewing { userParam ? `${user.username}'s` : 'your'} profile
         </h2>
+      {userParam && (
+        <button className="btn ml-auto" onClick={handleClick}>
+          Add Friend
+        </button>
+      )};
       </div>
 
       <div className="flex-row justify-space-between mb-3">
@@ -60,6 +80,7 @@ const Profile = () => {
           />
         </div>
       </div>
+      <div className='mb-3'>{!userParam && <ThoughtForm />}</div>
     </div>
   );
 };
